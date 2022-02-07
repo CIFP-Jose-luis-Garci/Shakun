637ce02c71ad42042121900cd8f04e4852a60511
Yaiza <yaizagh2001@gmail.com>
2022-02-07 12:14:10 +0100

    InputSystem
    Creando un InputSystem para los movimientos de Shonu

---
+++ Unity/Shakun_v.0.1/Assets/InputSystem/InputControl.cs
@@ -0,0 +1,172 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/InputSystem/InputControl.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @InputControl : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @InputControl()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""InputControl"",
+    ""maps"": [
+        {
+            ""name"": ""Moverse"",
+            ""id"": ""e3fb22ed-c4f2-4277-8d97-5a68b7ee1394"",
+            ""actions"": [
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""Value"",
+                    ""id"": ""40bd94f1-e23e-4c9e-9879-6d64511e80e5"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Run"",
+                    ""type"": ""Button"",
+                    ""id"": ""9a4a2d05-743c-4475-bc83-271989fba83f"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""12074c53-0142-4001-bb54-101fe44de81f"",
+                    ""path"": ""<Gamepad>/leftShoulder"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Run"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""33a53965-733e-4e94-864a-c9e17a833d86"",
+                    ""path"": ""<Keyboard>/leftCtrl"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Run"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""cb4b7ff7-d886-4ad0-8447-b4a525ef9309"",
+                    ""path"": ""<Gamepad>/leftStick"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Moverse
+        m_Moverse = asset.FindActionMap("Moverse", throwIfNotFound: true);
+        m_Moverse_Move = m_Moverse.FindAction("Move", throwIfNotFound: true);
+        m_Moverse_Run = m_Moverse.FindAction("Run", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Moverse
+    private readonly InputActionMap m_Moverse;
+    private IMoverseActions m_MoverseActionsCallbackInterface;
+    private readonly InputAction m_Moverse_Move;
+    private readonly InputAction m_Moverse_Run;
+    public struct MoverseActions
+    {
+        private @InputControl m_Wrapper;
+        public MoverseActions(@InputControl wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Move => m_Wrapper.m_Moverse_Move;
+        public InputAction @Run => m_Wrapper.m_Moverse_Run;
+        public InputActionMap Get() { return m_Wrapper.m_Moverse; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(MoverseActions set) { return set.Get(); }
+        public void SetCallbacks(IMoverseActions instance)
+        {
+            if (m_Wrapper.m_MoverseActionsCallbackInterface != null)
+            {
+                @Move.started -= m_Wrapper.m_MoverseActionsCallbackInterface.OnMove;
+                @Move.performed -= m_Wrapper.m_MoverseActionsCallbackInterface.OnMove;
+                @Move.canceled -= m_Wrapper.m_MoverseActionsCallbackInterface.OnMove;
+                @Run.started -= m_Wrapper.m_MoverseActionsCallbackInterface.OnRun;
+                @Run.performed -= m_Wrapper.m_MoverseActionsCallbackInterface.OnRun;
+                @Run.canceled -= m_Wrapper.m_MoverseActionsCallbackInterface.OnRun;
+            }
+            m_Wrapper.m_MoverseActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Move.started += instance.OnMove;
+                @Move.performed += instance.OnMove;
+                @Move.canceled += instance.OnMove;
+                @Run.started += instance.OnRun;
+                @Run.performed += instance.OnRun;
+                @Run.canceled += instance.OnRun;
+            }
+        }
+    }
+    public MoverseActions @Moverse => new MoverseActions(this);
+    public interface IMoverseActions
+    {
+        void OnMove(InputAction.CallbackContext context);
+        void OnRun(InputAction.CallbackContext context);
+    }
+}
--- Unity/Shakun_v.0.1/Assets/Scripts/ShonuMove.cs
+++ Unity/Shakun_v.0.1/Assets/Scripts/ShonuMove.cs
@@ -4,15 +4,100 @@ using UnityEngine;
 
 public class ShonuMove : MonoBehaviour
 {
+    InputControl inputcontrol;
+
+    bool corriendo = false;
+   // bool camninado = false;
+
+    Vector2 stickL;
+
+    Animator animator;
+
+    CharacterController cc;
+
+    float speed;
+
+    Vector3 dir;
+
+    private void Awake()
+    {
+        inputcontrol = new InputControl();
+
+
+
+        //Correr
+        inputcontrol.Moverse.Run.performed += ctx => { corriendo = true; };
+        inputcontrol.Moverse.Run.canceled += ctx => { corriendo = false; };
+
+        //Caminar
+        inputcontrol.Moverse.Move.performed += ctx => stickL = ctx.ReadValue<Vector2>();
+        inputcontrol.Moverse.Move.canceled += ctx => stickL = Vector2.zero;
+
+
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        cc = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (corriendo && stickL.y > 0)
+        {
+            Correr();
+
+        }
+        else
+        {
+            Caminar();
+        }
+
+        //Giro
+        transform.Rotate(new Vector3(0f, stickL.x * 0.8f, 0f));
+    }
+
+    void Correr()
+    {
+        animator.SetBool("RunShonu", true);
+        speed = 5f;
+        dir = transform.TransformDirection(Vector3.forward);
+        cc.SimpleMove(dir * speed * stickL.y);
+    }
+    void Caminar()
+    {
         
+        animator.SetBool("RunShonu", false);
+        animator.SetFloat("WalkShonu", stickL.y);
+
+        if (stickL.y > 0)
+        {
+            speed = 2.2f;
+        }
+        else
+        {
+            speed = 0.8f;
+        }
+
+        dir = transform.TransformDirection(Vector3.forward);
+
+        cc.SimpleMove(dir * speed * stickL.y);
+    }
+
+
+
+
+
+
+    private void OnEnable()
+    {
+        inputcontrol.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputcontrol.Disable();
     }
 }
